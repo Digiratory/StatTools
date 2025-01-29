ccffbdfb5a9c907614170da7da962f09ebcd8f1d
Asya Lyanova <lyanova.as@yandex.ru>
2025-01-29 16:45:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup, Extension
-from numpy import get_include
+from setuptools.command.build_ext import build_ext
 
-module = Extension('C_StatTools',
-                   include_dirs=[get_include()],
-                   sources=['StatTools_C_API.cpp'],
-                   language='c++')
+
+class CustomBuildExt(build_ext):
+    def finalize_options(self):
+        if self.include_dirs is None:
+            self.include_dirs = []
+
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+        super().finalize_options()
+
 
 requirements = [line.strip() for line in open("requirements.txt").readlines()]
 
 setup(
     name="StatTools",
     version="1.6.0",
-    ext_modules=[module],
+    ext_modules=[Extension('C_StatTools', sources=[
+                           'StatTools_C_API.cpp'], language='c++')],
+    cmdclass={'build_ext': CustomBuildExt},
     author="Alexandr Kuzmenko",
     author_email="alexander.k.spb@gmail.com",
     packages=["StatTools", "StatTools.analysis",
