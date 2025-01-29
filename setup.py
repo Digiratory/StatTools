57119217f1d3c45e6189070d0ff5f6a96d2e5eaf
Asya Lyanova <lyanova.as@yandex.ru>
2025-01-29 15:25:25 +0300

    build: upd setup

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-from setuptools import setup
-from numpy import get_include
 from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
 
-module = Extension('C_StatTools',
-                   include_dirs=[get_include()],
-                   sources=['StatTools_C_API.cpp'],
-                   language='c++')
 
-requirements = [line.strip() for line in open("requirements.txt").readlines()]
+class CustomBuildExt(build_ext):
+    def finalize_options(self):
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+        super().finalize_options()
+
+
+requirements = ['memory-profiler==0.58.0', 'numpy==1.22.4', 'psutil==5.8.0', 'Pympler==0.9', 'scipy==1.13.1',
+                'setuptools~=57.0.0', 'matplotlib~=3.5.1', 'seaborn~=0.11.2', 'tqdm~=4.62.3', 'Pillow~=8.4.0', 'pandas~=1.3.5', 'rich>=10.9.0']
 
 setup(
     name="StatTools",
     version="1.6.0",
-    ext_modules=[module],
+    ext_modules=[Extension('C_StatTools', sources=[
+                           'StatTools_C_API.cpp'], language='c++')],
+    cmdclass={'build_ext': CustomBuildExt},
     author="Alexandr Kuzmenko",
     author_email="alexander.k.spb@gmail.com",
     packages=["StatTools", "StatTools.analysis",
