45d147525ad5ea6288ee37ae1ea170d87d6d9a26
Asya Lyanova <64890333+pipipyau@users.noreply.github.com>
2025-01-31 18:33:08 +0300

    ci

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [line.strip() for line in open("requirements.txt").readlines()]
 
 setup(
     name="FluctuationAnalysisTools",
-    version="1.6.0a0",
+    version="1.6.1a0",
     ext_modules=[module],
     author="Alexandr Kuzmenko",
     author_email="alexander.k.spb@gmail.com",
